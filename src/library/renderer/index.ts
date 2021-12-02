d058b7b0c6c2f9f3313d0ef0ecc56e8f3ae1aa3e
iwanhae <iwanhae@gmail.com>
2021-12-03 00:27:10 +0900

    pictogram avatar

--- src/library/renderer/index.ts
+++
@@ -1,9 +0,0 @@
-import { WebGLRenderer, sRGBEncoding } from 'three';
-
-const renderer = new WebGLRenderer({ antialias: true });
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.outputEncoding = sRGBEncoding;
-renderer.shadowMap.enabled = true;
-
-export default renderer;
