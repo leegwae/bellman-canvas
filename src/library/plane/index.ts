b84776edf5c8bd60ef4ce222cfa1f438c4b9bf3a
leegwae <leegwae@gmail.com>
2021-12-03 05:52:36 +0900

    ui 예시 구성

--- src/library/plane/index.ts
+++ src/library/plane/index.ts
@@ -2,7 +2,7 @@ import { Mesh, PlaneGeometry, MeshPhongMaterial } from 'three';
 
 const plane = new Mesh(
   new PlaneGeometry(100, 100),
-  new MeshPhongMaterial({ color: 0x999999, depthWrite: false }),
+  new MeshPhongMaterial({ color: 'black', depthWrite: false }),
 );
 plane.rotation.x = -Math.PI / 2;
 plane.receiveShadow = true;
--- src/library/scene/index.ts
+++ src/library/scene/index.ts
@@ -1,7 +1,6 @@
 import { Scene, Color, Fog } from 'three';
 
 const scene = new Scene();
-scene.background = new Color(0xa0a0a0);
-scene.fog = new Fog(0xa0a0a0, 10, 50);
+scene.background = new Color('black');
 
 export default scene;
